09620d7ad90451fa18fbe928410e8d4507aa1a01
Ari <ari.s.email0210@gmail.com>
2026-07-23 20:02:56 +0800

    chore:关闭 Spring Data Redis 的自动仓储扫描

--- backend/src/main/java/com/arii/JobTracker/JobTrackerApplication.java
+++ backend/src/main/java/com/arii/JobTracker/JobTrackerApplication.java
@@ -3,12 +3,14 @@ package com.arii.JobTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableJpaAuditing 
 @SpringBootApplication
 @EnableScheduling // 定时器
+@EnableJpaRepositories(basePackages = "com.arii.JobTracker.Repository")
 public class JobTrackerApplication {
 
 	public static void main(String[] args) {
